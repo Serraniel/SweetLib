0df278d861966bed93eabe991bc4f1f66e1f0ca9
Serraniel <online-daniel95@web.de>
2017-10-26 19:02:43 +0200

    Added the storer implementations to the new project

---
+++ SweetLib/Classes/Exceptions/RegistryStorerException.cs
@@ -0,0 +1,21 @@
+﻿using SweetLib.Classes.Storer;
+
+namespace SweetLib.Classes.Exceptions
+{
+    /// <summary>
+    /// Exception thrown by <see cref="RegistryStorer"/>.
+    /// </summary>
+    public class RegistryStorerException : System.Exception
+    {
+        /// <summary>
+        /// Creates a new <see cref="RegistryStorerException"/>.
+        /// </summary>
+        public RegistryStorerException() { }
+
+        /// <summary>
+        /// Creates a new <see cref="RegistryStorerException"/>.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        public RegistryStorerException(string message) : base(message) { }
+    }
+}
