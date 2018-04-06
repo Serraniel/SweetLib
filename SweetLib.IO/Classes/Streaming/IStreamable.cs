d393bd184d70493f3705924d808ad66d61d7545c
Serraniel <serraniel@posteo.de>
2018-04-06 17:33:41 +0200

    Added interface for streamable objects

---
+++ SweetLib.IO/Classes/Streaming/IStreamable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SweetLib.IO.Classes.Streaming
+{
+    /// <summary>
+    /// Streamable objects
+    /// </summary>
+    public interface IStreamable
+    {
+        /// <summary>
+        /// Saves the current object into a stream.
+        /// </summary>
+        /// <param name="stream">Stream in which the current object should be saved.</param>
+        void SaveToStream(Stream stream);
+        
+        /// <summary>
+        /// Loads the object from a given stream.
+        /// </summary>
+        /// <param name="stream">Stream from which the object is loaded.</param>
+        void LoadFromStream(Stream stream);
+    }
+}
